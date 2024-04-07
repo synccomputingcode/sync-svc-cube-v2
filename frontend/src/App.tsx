3f8bc63e27175946943aa7599b21da364507df07
Samantha Hughes <shughes-uk@users.noreply.github.com>
2024-04-07 00:03:17 -0700

    contact (#3)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,90 @@
-import { Container } from "@mui/material";
-import underConstruction from "./assets/under-construction.gif";
+import {
+  Button,
+  Container,
+  Link,
+  Stack,
+  Table,
+  TableCell,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { CopyAll, GitHub, LinkedIn } from "@mui/icons-material";
+import { useCopyToClipboard } from "usehooks-ts";
+import { useSnackbar } from "notistack";
 
 function App() {
+  const [, copy] = useCopyToClipboard();
+  const { enqueueSnackbar } = useSnackbar();
+  const handleCopy = (text: string) => {
+    copy(text);
+    enqueueSnackbar("Copied to clipboard", { variant: "success" });
+  };
   return (
-    <Container maxWidth={"sm"}>
-      <img src={underConstruction} alt="Under Construction" width="100%" />
-    </Container>
+    <div>
+      <Container maxWidth={"sm"}>
+        <Typography variant="h2" align="center" gutterBottom>
+          Samantha Hughes
+        </Typography>
+        <Table>
+          <TableRow>
+            <TableCell>Email:</TableCell>
+            <TableCell>
+              <Link href="mailto:shughes.uk@gmail.com">
+                shughes.uk@gmail.com
+              </Link>
+            </TableCell>
+            <TableCell>
+              <Button onClick={() => handleCopy("shughes.uk@gmail.com")}>
+                <CopyAll />
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Phone:</TableCell>
+            <TableCell>
+              <Link href="tel:+15129099300">+1-512-909-9300</Link>
+            </TableCell>
+            <TableCell>
+              <Button onClick={() => handleCopy("+1-512-909-9300")}>
+                <CopyAll />
+              </Button>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Social:</TableCell>
+            <TableCell sx={{ alignContent: "center" }}>
+              <Stack direction="row" spacing={1} alignItems={"center"}>
+                <Link
+                  href="https://www.linkedin.com/in/samantha-hughes-2b8b7716"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <LinkedIn />
+                </Link>
+                <Link
+                  href="https://github.com/shughes-uk"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <GitHub htmlColor="black" />
+                </Link>
+              </Stack>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <Link
+                href="https://calendly.com/shughes-uk/30min"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Schedule a call
+              </Link>
+            </TableCell>
+          </TableRow>
+        </Table>
+      </Container>
+    </div>
   );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@fontsource/roboto";
+import { SnackbarProvider } from "notistack";
+import { CssBaseline } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>,
 );
