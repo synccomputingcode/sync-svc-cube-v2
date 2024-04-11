8955d12c45e30e1f261cc9237a7ede5054f7e695
shughes.uk <shughes.uk@gmail.com>
2024-04-11 15:01:22 -0700

    healthcheck schema

--- frontend/src/crud/misc/hooks.tsx
+++ frontend/src/crud/misc/hooks.tsx
@@ -1,8 +1,9 @@
 import { UseQueryResult, useQuery } from "react-query";
 import { ApiClient } from "../../api";
+import { HealthCheckSchema } from "../../api-client";
 
 export const useHealthcheck = (): UseQueryResult<void> => {
-  return useQuery<void>(["healthcheck"], ({ signal }) =>
+  return useQuery<HealthCheckSchema>(["healthcheck"], ({ signal }) =>
     ApiClient.resumeApiHello({
       signal,
     }),
