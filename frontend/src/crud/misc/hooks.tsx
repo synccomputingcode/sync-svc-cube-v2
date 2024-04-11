a08557b0f4b9c2f152c00731b3957016c01a166d
shughes.uk <shughes.uk@gmail.com>
2024-04-11 15:07:14 -0700

    tsc

--- frontend/src/crud/misc/hooks.tsx
+++ frontend/src/crud/misc/hooks.tsx
@@ -2,7 +2,7 @@ import { UseQueryResult, useQuery } from "react-query";
 import { ApiClient } from "../../api";
 import { HealthCheckSchema } from "../../api-client";
 
-export const useHealthcheck = (): UseQueryResult<void> => {
+export const useHealthcheck = (): UseQueryResult<HealthCheckSchema> => {
   return useQuery<HealthCheckSchema>(["healthcheck"], ({ signal }) =>
     ApiClient.resumeApiHello({
       signal,
