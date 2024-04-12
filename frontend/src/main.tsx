69d1bb720384eda4a26d9199f30b49127339adcd
shughes.uk <shughes.uk@gmail.com>
2024-04-11 23:42:23 -0700

    use google cdn for fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,3 @@
-import "@fontsource/roboto";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import { CssBaseline } from "@mui/material";
 import { SnackbarProvider } from "notistack";
 import React from "react";
