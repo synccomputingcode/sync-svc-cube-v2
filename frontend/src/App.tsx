b59f03dd0f02293737f165465798185637f0dea0
Samantha Hughes <samanthahughes@Samanthas-MacBook-Pro.local>
2024-04-18 13:04:40 -0700

    source code cta

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,42 @@
+import { GitHub } from "@mui/icons-material";
 import { LoginView } from "./pages/login";
+import { Link, Tooltip } from "@mui/material";
+import { useLocalStorage } from "usehooks-ts";
+import { useEffect } from "react";
 
 function App() {
-  return <LoginView />;
+  const [hasSeenSourceTooltip, setHasSeenSourceTooltip] =
+    useLocalStorage<boolean>("hasSeenSourceTooltip", false);
+  useEffect(() => {
+    if (!hasSeenSourceTooltip) {
+      setTimeout(() => {
+        setHasSeenSourceTooltip(true);
+      }, 5000);
+    }
+  });
+  return (
+    <>
+      <LoginView />
+
+      <Link component={Link} href="https://github.com/shughes-uk/resume">
+        <Tooltip
+          open={hasSeenSourceTooltip ? undefined : true}
+          title="Explore this project on GitHub!"
+          arrow
+        >
+          <GitHub
+            htmlColor="black"
+            sx={{
+              position: "fixed",
+              bottom: "20px",
+              right: "20px",
+              zIndex: 9999,
+            }}
+          />
+        </Tooltip>
+      </Link>
+    </>
+  );
 }
 
 export default App;
