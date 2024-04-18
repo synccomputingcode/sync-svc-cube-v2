794659374e4668800bbc4e3bb65aa27d471fafd2
Samantha Hughes <samanthahughes@Samanthas-MacBook-Pro.local>
2024-04-18 12:40:29 -0700

    resume links

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,4 +1,11 @@
-import { GitHub, Group, LinkedIn, Mail, Phone } from "@mui/icons-material";
+import {
+  Download,
+  GitHub,
+  Group,
+  LinkedIn,
+  Mail,
+  Phone,
+} from "@mui/icons-material";
 import {
   Box,
   Button,
@@ -11,16 +18,20 @@ import {
   ListItemIcon,
   ListItemSecondaryAction,
   ListItemText,
+  Menu,
+  MenuItem,
   Stack,
   Typography,
 } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { CopyButton } from "../components/CopyButton";
-import { useHealthcheck } from "../crud/misc/hooks";
+import { useRef, useState } from "react";
+import PDFResume from "../assets/Samantha_Hughes_Resume.pdf";
+import DocXResume from "../assets/Samantha_Hughes_Resume.docx";
 
 export function LoginView(): React.ReactElement {
-  const { data: barf, isSuccess } = useHealthcheck();
-  console.log("DATA", barf, isSuccess);
+  const [isDownloadMenuOpen, setIsDownloadMenuOpen] = useState(false);
+  const downloadMenuAnchorRef = useRef(null);
   return (
     <Box
       sx={{
@@ -73,84 +84,135 @@ export function LoginView(): React.ReactElement {
             minWidth: "fit-content",
           }}
         >
-          <Card elevation={10}>
-            <CardContent>
-              <Stack spacing={1}>
-                <Stack>
+          <Stack spacing={2}>
+            <Card elevation={10}>
+              <CardContent>
+                <Stack spacing={1}>
                   <Typography
                     variant="h5"
                     sx={{ fontWeight: 600, whiteSpace: "nowrap" }}
                   >
                     Get In Touch
                   </Typography>
+                  <Divider />
+                  <List dense>
+                    <ListItem>
+                      <ListItemIcon>
+                        <Mail />
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={
+                          <Link href="mailto:shughes.uk@gmail.com">
+                            shughes.uk@gmail.com
+                          </Link>
+                        }
+                      />
+                      <ListItemSecondaryAction>
+                        <CopyButton text="shughes.uk@gmail.com" />
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
+                      <ListItemIcon>
+                        <Phone />
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={
+                          <Link href="tel:+15129099300">+1-512-909-9300</Link>
+                        }
+                      />
+                      <ListItemSecondaryAction>
+                        <CopyButton text="+1-512-909-9300" />
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
+                      <ListItemIcon>
+                        <Group />
+                      </ListItemIcon>
+                      <ListItemIcon>
+                        <Stack
+                          direction="row"
+                          spacing={1}
+                          alignItems={"center"}
+                        >
+                          <Link
+                            href="https://www.linkedin.com/in/samantha-hughes-2b8b7716"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <LinkedIn />
+                          </Link>
+                          <Link
+                            href="https://github.com/shughes-uk"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <GitHub htmlColor="black" />
+                          </Link>
+                        </Stack>
+                      </ListItemIcon>
+                    </ListItem>
+                  </List>
+                  <Button
+                    size="small"
+                    variant="outlined"
+                    ref={downloadMenuAnchorRef}
+                    onClick={() => setIsDownloadMenuOpen(true)}
+                  >
+                    Download Resume
+                  </Button>
+                  <Menu
+                    open={isDownloadMenuOpen}
+                    onClose={() => setIsDownloadMenuOpen(false)}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "left",
+                    }}
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "left",
+                    }}
+                    anchorEl={downloadMenuAnchorRef.current}
+                    keepMounted
+                  >
+                    <MenuItem
+                      component={Link}
+                      href={DocXResume}
+                      download
+                      target="_blank"
+                      onClick={() => setIsDownloadMenuOpen(false)}
+                    >
+                      <ListItemIcon>
+                        <Download />
+                      </ListItemIcon>
+                      <ListItemText primary="Microsoft Word (.docx)" />
+                    </MenuItem>
+                    <MenuItem
+                      component={Link}
+                      href={PDFResume}
+                      download
+                      target="_blank"
+                      onClick={() => setIsDownloadMenuOpen(false)}
+                    >
+                      <ListItemIcon>
+                        <Download />
+                      </ListItemIcon>
+                      <ListItemText primary="PDF Document (.pdf)" />
+                    </MenuItem>
+                  </Menu>
+                  <Button
+                    component={Link}
+                    size="small"
+                    variant="contained"
+                    href="https://calendly.com/shughes-uk/30min"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Schedule a call
+                  </Button>
                 </Stack>
-                <Divider />
-                <List dense>
-                  <ListItem>
-                    <ListItemIcon>
-                      <Mail />
-                    </ListItemIcon>
-                    <ListItemText
-                      primary={
-                        <Link href="mailto:shughes.uk@gmail.com">
-                          shughes.uk@gmail.com
-                        </Link>
-                      }
-                    />
-                    <ListItemSecondaryAction>
-                      <CopyButton text="shughes.uk@gmail.com" />
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemIcon>
-                      <Phone />
-                    </ListItemIcon>
-                    <ListItemText
-                      primary={
-                        <Link href="tel:+15129099300">+1-512-909-9300</Link>
-                      }
-                    />
-                    <ListItemSecondaryAction>
-                      <CopyButton text="+1-512-909-9300" />
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemIcon>
-                      <Group />
-                    </ListItemIcon>
-                    <ListItemIcon>
-                      <Stack direction="row" spacing={1} alignItems={"center"}>
-                        <Link
-                          href="https://www.linkedin.com/in/samantha-hughes-2b8b7716"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <LinkedIn />
-                        </Link>
-                        <Link
-                          href="https://github.com/shughes-uk"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <GitHub htmlColor="black" />
-                        </Link>
-                      </Stack>
-                    </ListItemIcon>
-                  </ListItem>
-                </List>
-                <Button
-                  component={Link}
-                  size="small"
-                  variant="contained"
-                  href="https://calendly.com/shughes-uk/30min"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Schedule a call
-                </Button>
-              </Stack>
-            </CardContent>
-          </Card>
+              </CardContent>
+            </Card>
+          </Stack>
         </Grid2>
       </Grid2>
     </Box>
