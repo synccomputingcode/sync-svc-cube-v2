6e1e4be71237a070b4680191e4c42e993b33d84f
Samantha Hughes <shughes.uk@gmail.com>
2024-04-21 17:50:41 -0700

    fix allauth settings so email is requested

--- backend/resume/settings.py
+++ backend/resume/settings.py
@@ -185,3 +185,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+SOCIALACCOUNT_EMAIL_REQUIRED = True
+SOCIALACCOUNT_QUERY_EMAIL = True
